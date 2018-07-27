4df41a9cb58d422c820f92a5667a33e6142952a4
DavidZ <david.zafirovic@gmail.com>
2018-07-27 15:34:26 -0400

    Did user auth

--- models/user.js
+++ models/user.js
@@ -5,13 +5,18 @@ const User = new Schema({
   local: {
     email: String,
     password: String
-  },
-  tweets: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Tweet"
-    }
-  ]
+  }
 });
+
+module.exports = mongoose.model("User", User);
+
+
+User.methods.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+User.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
 
 module.exports = mongoose.model("User", User);
